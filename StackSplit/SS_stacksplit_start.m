4a924363191d65c8c76932b47aa7fde3c36842d7
Yvonne Fröhlich <yfroe@gpiseis16.gpi.kit.edu>
2023-01-13 11:00:45 +0100

    Complete reference 'Wuestefeld et al 2008' in 'SS_stacksplit_start.m'

--- StackSplit/SS_stacksplit_start.m
+++ StackSplit/SS_stacksplit_start.m
@@ -55,7 +55,7 @@ function SS_stacksplit_start
 %     https://doi.org/10.1016/j.cageo.2017.04.015.
 %
 % Wüstefeld et al. (2008), SplitLab: A shear-wave splitting environment in Matlab, 
-%     Computers & Geosciences 34, 515–528
+%     Computers & Geosciences 34, 515–528, https://doi.org/10.1016/j.cageo.2007.08.002.
 %
 %.................................................
 % USED METHODS (depending on your application)
