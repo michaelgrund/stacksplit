9938f79681b159362acd18005ee05b5be1232ad2
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-09-10 08:13:06 +0200

    Merge queries into one query

--- StackSplit/SS_check_matlab_version.m
+++ StackSplit/SS_check_matlab_version.m
@@ -67,6 +67,7 @@ vers_let = vers(5);
 
 % Do NOT change the oder of these queries!
 
+%{
 vers_out = 0
 
 % (I) Applying the contourf function to create the energy maps
@@ -85,6 +86,29 @@ end
 if (vers_yyyy==2023 && strcmp(vers_let,'b')) || vers_yyyy>2023  % R2023b and higher
     vers_out = 3;
 end
+%}
+
+vers_out = 0
+
+% (I) Applying the contourf function to create the energy maps
+if (vers_yyyy==2014 && strcmp(vers_let,'b')) || vers_yyyy>2014  % R2014b and higher
+   vers_out = 1;
+
+    % YF 2023-01-04
+    % (II) Using the coastlines provided by the Mapping Toolbox
+    if (vers_yyyy==2020 && strcmp(vers_let,'b')) || vers_yyyy>2020  % R2020b and higher
+        vers_out = 2;
+
+        % YF 2023-08-16
+        % (III) Using "imresize" instead of "resizem"
+        if (vers_yyyy==2023 && strcmp(vers_let,'b')) || vers_yyyy>2023  % R2023b and higher
+            vers_out = 3;
+        end
+
+    end
+
+end
+
 
 %==========================================================================
 %==========================================================================
