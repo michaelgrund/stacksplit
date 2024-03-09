c78b368536c2150670d59116ddd3c1bd676daac7
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-03-09 19:42:23 +0100

    Improve variable name

--- StackSplit/SL_mod/SL_1.0.5_mod/splitlab_SS.m
+++ StackSplit/SL_mod/SL_1.0.5_mod/splitlab_SS.m
@@ -11,8 +11,8 @@ config.version='SplitLab1.0.4';
 
 % Modification due to StackSplit up on v3.1
 % Add modified function checkmattaupclass from Splitlab 1.9.0 for matTaup; YF 22.12.2019
-ok = checkmattaupclass;
-if ok==0
+taup_ok = checkmattaupclass;
+if taup_ok==0
     warning('Troubles loading matTaup')
 end
 
